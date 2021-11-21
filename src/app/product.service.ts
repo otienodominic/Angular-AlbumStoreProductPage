ba22ceaf6abf3430aa8cee659492af15e1cd4dc3
Dominic Ngalo <otienodominic@gmail.com>
2021-11-21 11:28:50 +0300

    Using Interface to Describe Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,6 +8,6 @@ export class ProductService {
   private _albumUrl: string = '../assets/album.json'
   constructor(private _http: Http) { }
   getAlbum(id: number): Observable<Album>{
-    return this._http.get(this._albumUrl).map(response => response.json() as Album);
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
